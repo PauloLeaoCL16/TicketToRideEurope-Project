85c86a5836a0ef1ac82c3652b9f7129d7fdf4823
PauloLeaoCL16 <k2117991@students.katyisd.org>
2025-03-05 10:30:55 -0600

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,15 +1,38 @@
-import java.util.BufferedImage;
-import javax.imageio.ImageIO;
+import java.util.*;
+import java.awt.image.*;
 
-public class Ticket {
-    private String fromCity;
-    private String toCity;
-    private int points;
-    private BufferedImage image;
-    public Ticket(String start, String end, int points, String directory) {
-        fromCity = start;
-        toCity = end;
-        this.points = points;
-        image = ImageIO.read(new File(directory));
-    }
+public class Ticket 
+{
+	String toCity;
+	String fromCity;
+	int points;
+	BufferedImage image;
+	
+	public Ticket(String toCity, String fromCity, int points, BufferedImage image)
+	{
+		this.toCity = toCity;
+		this.fromCity = fromCity;
+		this.points = points;
+		this.image = image;
+	}
+	
+	public String getToCity()
+	{
+		return toCity;
+	}
+	
+	public String getFromCity()
+	{
+		return fromCity;
+	}
+	
+	public int getPoints()
+	{
+		return points;
+	}
+	
+	public BufferedImage getImage()
+	{
+		return image;
+	}
 }
