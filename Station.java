2872d313aee08094af1ce6b61f97088d69fd23a1
PauloLeaoCL16 <k2117991@students.katyisd.org>
2025-03-06 11:21:08 -0600

    Update Station.java

--- Station.java
+++ Station.java
@@ -1,6 +1,34 @@
-public class Station {
-    private String toCity;
-    public Station() {
+import java.util.*;
 
-    }
+public class Station 
+{
+	private String toCity;
+	private String fromCity;
+	
+	public Station()
+	{
+		toCity = "";
+		fromCity = "";
+	}
+	
+	public String getToCity()
+	{
+		return toCity;
+	}
+	
+	public String getFromCity()
+	{
+		return fromCity;
+	}
+	
+	public void setToCity(String city)
+	{
+		toCity = city;
+	}
+	
+	public void setFromCity(String city)
+	{
+		fromCity = city;
+	}
+	
 }
