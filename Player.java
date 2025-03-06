4ac2db3875a62ffda3a15dc5d9806c40bbf0a00c
PauloLeaoCL16 <k2117991@students.katyisd.org>
2025-03-06 11:20:43 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,17 +1,29 @@
 import java.util.*;
 
 public class Player {
+	
     private int points;
     private ArrayList<Ticket> tickets; //(Ex: {[Berlin, Amsterdam]=8}
     private ArrayList<ColorCard> card;
-    private int station;
+    private ArrayList<City> railRoadsBought;
+    private ArrayList<Station> usedStationList;
+    private ArrayList<Station> unusedStationList;
     private int trainsLeft;
+    
     public Player() {
-        station = 3;
         points = 0;
         trainsLeft = 45;
         tickets = new ArrayList<Ticket>();
         card = new ArrayList<ColorCard>();
+        unusedStationList.add(new Station());
+        unusedStationList.add(new Station());
+        unusedStationList.add(new Station());
+    }
+    
+    public void useStation()
+    {
+    	if(unusedStationList.size()>=1)
+    		usedStationList.add(unusedStationList.remove(unusedStationList.size()-1));
     }
 
     public void addTicket(Ticket ticket) {
@@ -24,10 +36,6 @@ public class Player {
 
     public ArrayList<Ticket> getTicket() {
         return tickets;
-    }
-
-    public int getStation() {
-        return station;
     }
 
     public void setPoint(int pt) {
@@ -45,13 +53,23 @@ public class Player {
     public ArrayList<ColorCard> getCard() {
         return card;
     }
+    
+    public void removeCards(String color, int num)
+    {
+    	for(ColorCard cardRemove : card)
+    	{
+	    	if(cardRemove.getColor().equals(color) && num>0)
+	    	{
+	    		card.remove(cardRemove);
+	    		num--;
+	    	}
+    	}
+    }
 
-    public void removeTrain(int numberToRemove) {
-        if (trainsLeft - numberToRemove <= 2) {
-            System.out.print("Not enough train");
-            return;
-        }
-        trainsLeft -= numberToRemove;
+    public void removeTrain(int numberToRemove)
+    {
+    	if( trainsLeft - numberToRemove >= 0)
+    		trainsLeft -= numberToRemove;
     }
 
     public int getTrainsLeft() {
