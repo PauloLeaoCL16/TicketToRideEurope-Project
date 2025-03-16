311f62104e5d71006fbd405ad3fcac8d2e8f913f
krushy <k2109441@students.katyisd.org>
2025-03-15 23:59:26 -0500

    Update ttreFrame.java

--- ttreFrame.java
+++ ttreFrame.java
@@ -1,20 +1,25 @@
-import java.awt.*;
+package ttreImages;
+
 import javax.swing.*;
-import java.awt.image.*;
-import javax.imageio.ImageIO;
-import java.io.*;
-public class ttreFrame extends JFrame{
-	private static final int WIDTH = 1920;
-	private static final int HEIGHT = 1080;
-	
-	public ttreFrame(String framename)
-	{
-		super(framename);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(WIDTH,HEIGHT);
-		setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		setUndecorated(false);
-		//add(new MainMenu(true));
-		setVisible(true);
-	}
+import java.awt.*;
+
+public class ttreFrame extends JFrame {
+    private static final int WIDTH = 1920;
+    private static final int HEIGHT = 1080;
+
+    public ttreFrame(String framename) {
+        super(framename);
+ 
+
+        setSize(WIDTH, HEIGHT);
+        setResizable(false);
+
+        //setLocationRelativeTo(null);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        add(new MainMenu());
+
+        setVisible(true);
+    }
 }
