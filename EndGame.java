ebda802a5ede4fe25b72abcaf4c8adede2abd9d2
Quan <111527496+VuAnhQua@users.noreply.github.com>
2025-03-19 11:21:35 -0500

    Update EndGame.java

--- EndGame.java
+++ EndGame.java
@@ -1,1 +1,41 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
+public class EndGame extends JPanel implements Runnable, MouseListener, MouseMotionListener  {
+	public EndGame(Player plr1, Player plr2, Player plr3, Player plr4) {
+		
+	}
+	
+	@Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+      
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {}
+
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
+    
+    @Override
+    public void run() {}
+}
