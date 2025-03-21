13e843cbc257c57e5ebb750a6535899bcfcd6fdc
Quan <111527496+VuAnhQua@users.noreply.github.com>
2025-03-21 10:54:14 -0500

    Update CardDeck.java

--- CardDeck.java
+++ CardDeck.java
@@ -19,31 +19,31 @@ public class CardDeck {
     private void add() {
     	try {
 	        for (int i = 0; i < 12; i++) {
-	            deck.add(new ColorCard("pink", ImageIO.read(MainMenu.class.getResource("/ttreImages/ticket.png"))));
+	            deck.add(new ColorCard("pink", ImageIO.read(MainMenu.class.getResource("/cards/purplecard.png"))));
 	        }
 	        for (int i = 0; i < 12; i++) {
-	            deck.add(new ColorCard("white", ImageIO.read(MainMenu.class.getResource("/ttreImages/ticket.png"))));
+	            deck.add(new ColorCard("white", ImageIO.read(MainMenu.class.getResource("/cards/whitecard.png"))));
 	        }
 	        for (int i = 0; i < 12; i++) {
-	            deck.add(new ColorCard("blue", ImageIO.read(MainMenu.class.getResource("/ttreImages/ticket.png"))));
+	            deck.add(new ColorCard("blue", ImageIO.read(MainMenu.class.getResource("/cards/bluecard.png"))));
 	        }
 	        for (int i = 0; i < 12; i++) {
-	            deck.add(new ColorCard("yellow", ImageIO.read(MainMenu.class.getResource("/ttreImages/ticket.png"))));
+	            deck.add(new ColorCard("yellow", ImageIO.read(MainMenu.class.getResource("/cards/yellowcard.png"))));
 	        }
 	        for (int i = 0; i < 12; i++) {
-	            deck.add(new ColorCard("orange", ImageIO.read(MainMenu.class.getResource("/ttreImages/ticket.png"))));
+	            deck.add(new ColorCard("orange", ImageIO.read(MainMenu.class.getResource("/cards/browncard.png"))));
 	        }
 	        for (int i = 0; i < 12; i++) {
-	            deck.add(new ColorCard("black", ImageIO.read(MainMenu.class.getResource("/ttreImages/ticket.png"))));
+	            deck.add(new ColorCard("black", ImageIO.read(MainMenu.class.getResource("/cards/blackcard.png"))));
 	        }
 	        for (int i = 0; i < 12; i++) {
-	            deck.add(new ColorCard("red", ImageIO.read(MainMenu.class.getResource("/ttreImages/ticket.png"))));
+	            deck.add(new ColorCard("red", ImageIO.read(MainMenu.class.getResource("/cards/redcard.png"))));
 	        }
 	        for (int i = 0; i < 12; i++) {
-	            deck.add(new ColorCard("green", ImageIO.read(MainMenu.class.getResource("/ttreImages/ticket.png"))));
+	            deck.add(new ColorCard("green", ImageIO.read(MainMenu.class.getResource("/cards/greencard.png"))));
 	        }
 	        for (int i = 0; i < 14; i++) {
-	            deck.add(new ColorCard("wild", ImageIO.read(MainMenu.class.getResource("/ttreImages/ticket.png"))));
+	            deck.add(new ColorCard("wild", ImageIO.read(MainMenu.class.getResource("/cards/wildcard.png"))));
 	        }
 	        shuffle();
 	    } catch (IOException e) {
