8df6c492790224f6c1f7a9ae4339c992b9283fb3
Quan <111527496+VuAnhQua@users.noreply.github.com>
2025-03-24 11:24:21 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,96 +1,159 @@
-import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
 
-public class Player {
-	
-    private int points;
-    private ArrayList<Ticket> tickets; //(Ex: {[Berlin, Amsterdam]=8}
-    private ArrayList<ColorCard> card;
-    private ArrayList<City> railRoadsBought;
-    private ArrayList<Station> usedStationList;
-    private ArrayList<Station> unusedStationList;
-    private int trainsLeft;
-    private String plrColor;
+public class GameBoard extends JPanel implements Runnable, MouseListener, MouseMotionListener {
+    private BufferedImage table, board, player1, player2, player3, player4, cardBack, ticket, template, redplayer, blueplayer, greenplayer, yellowplayer, playerpointer;
+    private boolean isPlayButtonHovered = false;
+    private boolean isRulesScrollHovered = false;
+    private ColorCard[] faceUpCard;
+    private CardDeck cardDeck;
+    private ArrayList<Player> players;
     
-    public Player(String plrColor) {
-        points = 0;
-        trainsLeft = 45;
-        tickets = new ArrayList<Ticket>();
-        card = new ArrayList<ColorCard>();
-        unusedStationList = new ArrayList<Station>();
-        unusedStationList.add(new Station());
-        unusedStationList.add(new Station());
-        unusedStationList.add(new Station());
-        railRoadsBought = new ArrayList<City>();
-        this.plrColor = plrColor;
-    }
-    
-    public void useStation()
-    {
-    	if(unusedStationList.size()>=1)
-    		usedStationList.add(unusedStationList.remove(unusedStationList.size()-1));
-    }
-    
-    public String getColor() {
-    	return plrColor;
-    }
-
-    public void addTicket(Ticket ticket) {
-        tickets.add(ticket);
-    }
-
-    public void removeTicket(Ticket ticket) {
-        tickets.remove(ticket);
-    }
-
-    public ArrayList<Ticket> getTicket() {
-        return tickets;
-    }
-
-    public void setPoint(int pt) {
-        points = pt;
-    }
-
-    public int getPoint() {
-        return points;
+    private int currentPlr;
+    public GameBoard() {
+        try {
+            table = ImageIO.read(MainMenu.class.getResource("/ttreImages/gameBackground.png"));
+            board = ImageIO.read(MainMenu.class.getResource("/ttreImages/gameboard.png"));
+            player1 = ImageIO.read(MainMenu.class.getResource("/ttreImages/player1label.png"));
+            player2 = ImageIO.read(MainMenu.class.getResource("/ttreImages/player2label.png"));
+            player3 = ImageIO.read(MainMenu.class.getResource("/ttreImages/player3label.png"));
+            player4 = ImageIO.read(MainMenu.class.getResource("/ttreImages/player4label.png"));
+            cardBack = ImageIO.read(MainMenu.class.getResource("/ttreImages/backofcard.png"));
+            ticket = ImageIO.read(MainMenu.class.getResource("/ttreImages/ticket.png"));
+            template = ImageIO.read(MainMenu.class.getResource("/ttreImages/blankcardtemp.png"));
+            redplayer = ImageIO.read(MainMenu.class.getResource("/ttreImages/redplayer.png"));
+            blueplayer = ImageIO.read(MainMenu.class.getResource("/ttreImages/blueplayer.png"));
+            greenplayer = ImageIO.read(MainMenu.class.getResource("/ttreImages/greenplayer.png"));
+            yellowplayer = ImageIO.read(MainMenu.class.getResource("/ttreImages/yellowplayer.png"));
+            playerpointer = ImageIO.read(MainMenu.class.getResource("/ttreImages/currentplayerarrow.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        currentPlr = 0;
+        
+        cardDeck = new CardDeck();
+        faceUpCard = new ColorCard[5];
+        faceUpCard[0] = cardDeck.drawCard();
+        faceUpCard[1] = cardDeck.drawCard();
+        faceUpCard[2] = cardDeck.drawCard();
+        faceUpCard[3] = cardDeck.drawCard();
+        faceUpCard[4] = cardDeck.drawCard();
+        
+        players = new ArrayList<Player>();
+        players.add(new Player("red"));
+        players.add(new Player("blue"));
+        players.add(new Player("green"));
+        players.add(new Player("yellow"));
+        
+        addMouseListener(this);
+        addMouseMotionListener(this);
+        setFocusable(true);
     }
 
-    public void addCard(ColorCard card) {
-        this.card.add(card);
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.drawImage(table, 0, 0, getWidth(), getHeight(), null);
+        g2d.drawImage(board, 0, 0, 1500, 1040, null);
+        g2d.drawImage(cardBack, 1560, 3, null);
+        g2d.drawImage(template, 1675, 3, null);
+        g2d.drawImage(ticket, 1795, 3, null);
+        
+        //rotate template cardholder and scale it smaller a bit
+//        g2d.scale(0.8, 0.8);
+//        g2d.rotate(Math.toRadians(-90), 1520, 240);
+        for (int i = 0; i < 5; i++) {
+        	g2d.drawImage(faceUpCard[i].getImage(), getWidth() - 360, 220 + i*80, 120, 80, null);
+        }
+        
+        //reset those transformations
+        g2d.setTransform(new AffineTransform());
+        
+        //numerical stuff
+        g2d.setFont(new Font("Serif", Font.BOLD, 50)); g2d.setColor(Color.WHITE); 
+        
+        //red player station and trains
+        g2d.drawString("0", 1758, 285);
+        g2d.drawString("0", 1828, 325); 
+        //green player station and trains
+        g2d.drawString("0", 1758, 385);
+        g2d.drawString("0", 1828, 420);
+        //yellow player station and trains
+        g2d.drawString("0", 1758, 480);
+        g2d.drawString("0", 1825, 520);
+        //blue player station and trains
+        g2d.drawString("0", 1752, 580);
+        g2d.drawString("0", 1822, 620);
+        
+        //number of trains stuff
+        g2d.setFont(new Font("Serif", Font.BOLD, 30)); g2d.setColor(Color.WHITE); 
+        g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("locomotive")), 1580, 790); // Locomotive
+        g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("white")), 1645, 790); // White
+        g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("red")), 1712, 790); // Red
+        g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("purple")), 1777, 790); // Purple
+        g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("green")), 1845, 790); // Green
+        //2nd row
+        g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("brown")), 1580, 930); // Brown
+        g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("blue")), 1645, 930); // Blue
+        g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("black")), 1712, 930); // Black
+        g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("yellow")), 1777, 930); // Yellow
+        g2d.drawString("9", 1845, 930); // Ticket
+        
+        //current player stuff
+        
+        //current player number
+        g2d.drawImage(player1, 1825, 948, null);
+        //current player arrow
+        g2d.drawImage(playerpointer, 1875, 270, null);
+        //current player's color
+        g2d.drawImage(redplayer, 1520, 925, null);
     }
 
-    public ArrayList<ColorCard> getCard() {
-        return card;
-    }
-    
-    public int getCardColor(String color) {
-    	int total = 0;
-    	for (int i = 0; i< card.size(); i++) {
-    		if (card.get(i).getColor().equals(color)) {
-    			total += 1;
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    	int x = e.getX();
+    	int y = e.getY();
+    	System.out.println(x + " " + y);
+    	for (int i = 0; i < 5; i++) {
+    		if (x > getWidth()-360 && x < getWidth()-240 && y> 220+i*80 && y<300+i*80) {
+    			players.get(currentPlr).addCard(faceUpCard[i]);
+    			faceUpCard[i] = cardDeck.drawCard();
     		}
-    	}
-    	return total;
-    }
-    
-    public void removeCards(String color, int num)
-    {
-    	for(ColorCard cardRemove : card)
-    	{
-	    	if(cardRemove.getColor().equals(color) && num>0)
-	    	{
-	    		card.remove(cardRemove);
-	    		num--;
-	    	}
-    	}
+        }
+    	repaint();
     }
 
-    public void removeTrain(int numberToRemove)
-    {
-    	if( trainsLeft - numberToRemove >= 0)
-    		trainsLeft -= numberToRemove;
+    @Override
+    public void mouseMoved(MouseEvent e) {
+      
     }
 
-    public int getTrainsLeft() {
-        return trainsLeft;
-    }
+    @Override
+    public void mouseDragged(MouseEvent e) {}
+
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
+
+    @Override
+    public void run() {}
 }
+
