0b1ff44ad0585e6b106353a951f42fb03444fbdb
Quan <111527496+VuAnhQua@users.noreply.github.com>
2025-03-25 17:00:28 -0500

    Update TicketDeck.java

--- TicketDeck.java
+++ TicketDeck.java
@@ -1,39 +1,31 @@
 package ttreImages;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.awt.image.*;
 
-public class Ticket 
-{
-	private String toCity;
-	private String fromCity;
-	private int points;
-	private BufferedImage image;
-	
-	public Ticket(String toCity, String fromCity, int points, BufferedImage image)
-	{
-		this.toCity = toCity;
-		this.fromCity = fromCity;
-		this.points = points;
-		this.image = image;
+public class TicketDeck {
+	private ArrayList<Ticket> ticketList;
+	public TicketDeck() {
+		ticketList = new ArrayList<Ticket>();
+		add();
 	}
 	
-	public String getToCity()
-	{
-		return toCity;
+	private void add() {
+		ticketList.add(new Ticket("Amsterdam", "Pamblona", 7, new BufferedImage(1, 1, 1)));
+		shuffle();
 	}
 	
-	public String getFromCity()
-	{
-		return fromCity;
-	}
+	private void shuffle() {
+        Collections.shuffle(ticketList);
+    }
+
 	
-	public int getPoints()
-	{
-		return points;
-	}
-	
-	public BufferedImage getImage()
-	{
-		return image;
+	public Ticket draw() {
+		Ticket item = ticketList.remove(ticketList.size() - 1);
+        if (ticketList.size() == 0) {
+            add();
+        }
+        
+        return item;
 	}
 }
