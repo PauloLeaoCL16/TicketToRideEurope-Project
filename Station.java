29d093ef63d4c444b6224c30e3a304aeaa3d5160
PauloLeaoCL16 <k2117991@students.katyisd.org>
2025-04-01 10:56:53 -0500

    Update Station.java

--- Station.java
+++ Station.java
@@ -1,14 +1,18 @@
+package ttreImages;
+
 import java.util.*;
 
 public class Station 
 {
 	private String toCity;
 	private String fromCity;
+	private String color;
 	
-	public Station()
+	public Station(String color)
 	{
 		toCity = "";
 		fromCity = "";
+		this.color = color;
 	}
 	
 	public String getToCity()
@@ -19,6 +23,10 @@ public class Station
 	public String getFromCity()
 	{
 		return fromCity;
+	}
+	
+	public String getColor() {
+		return color;
 	}
 	
 	public void setToCity(String city)
