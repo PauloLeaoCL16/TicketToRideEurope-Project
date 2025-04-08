63618f40fde7907f88488449a6059d66fd8c7ccb
PauloLeaoCL16 <k2117991@students.katyisd.org>
2025-04-08 10:48:22 -0500

    Update City.java

--- City.java
+++ City.java
@@ -6,9 +6,15 @@ public class City
 {
 	private String name;
 	private HashMap<City, ArrayList<RailRoad>> connections;
+	private HashMap<City, ArrayList<RailRoad>> secondConnections;
 	private Station hasStation;
 	private int x;
 	private int y;
+	
+	public City(String name)
+	{
+		
+	}
 	
 	public City(String name, Station station, int x, int y)
 	{
@@ -17,11 +23,17 @@ public class City
 		this.x = x;
 		this.y = y;
 		connections = new HashMap<>();
+		secondConnections = new HashMap<>();
 	}
 	
 	public void addConnection(City city, ArrayList<RailRoad> railRoad)
 	{
 		connections.put(city, railRoad);
+	}
+	
+	public void addSecondConnection(City city, ArrayList<RailRoad> railRoad)
+	{
+		secondConnections.put(city, railRoad);
 	}
 	
 	public String getName()
@@ -32,6 +44,11 @@ public class City
 	public HashMap<City, ArrayList<RailRoad>> getConnections()
 	{
 		return connections;
+	}
+	
+	public HashMap<City, ArrayList<RailRoad>> getSecondConnections()
+	{
+		return secondConnections;
 	}
 	
 	public void addStation(Station station)
