28e35eed3ad0aae30b5c07ba36a87d024d75786f
PauloLeaoCL16 <k2117991@students.katyisd.org>
2025-04-10 11:09:22 -0500

    Update TicketDeck.java

--- TicketDeck.java
+++ TicketDeck.java
@@ -1,22 +1,56 @@
 package ttreImages;
 import java.util.ArrayList;
 import java.util.Collections;
+
+import javax.imageio.ImageIO;
+
 import java.awt.image.*;
+import java.io.IOException;
 
 public class TicketDeck {
 	private ArrayList<Ticket> ticketList;
+	private ArrayList<Ticket> longTicketList;
+	
 	public TicketDeck() {
 		ticketList = new ArrayList<Ticket>();
+		longTicketList = new ArrayList<Ticket>();
 		add();
 	}
 	
 	private void add() {
-		ticketList.add(new Ticket("Amsterdam", "Pamblona", 7, new BufferedImage(1, 1, 1))); //Example (Need directory pls)
-		shuffle();
+		try 
+		{
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			ticketList.add(new Ticket("amsterdam", "pamplona", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/amsterdam-pamplona.png"))));
+			shuffle();
+		}
+		catch (IOException e) {
+            e.printStackTrace();
+        }
+		
 	}
 	
 	private void shuffle() {
         Collections.shuffle(ticketList);
+        Collections.shuffle(longTicketList);
     }
 
 	
@@ -28,4 +62,15 @@ public class TicketDeck {
         
         return item;
 	}
+	
+	public Ticket drawLongTicket() {
+		Ticket item = longTicketList.remove(longTicketList.size() - 1);
+        if (longTicketList.size() == 0) {
+            add();
+        }
+        
+        return item;
+	}
+	
+	
 }
