940b3706ec08facd18a7ba210705053c250055cb
CaioMAL <k2207212@students.katyisd.org>
2025-04-17 10:54:23 -0500

    Update TicketDeck.java

--- TicketDeck.java
+++ TicketDeck.java
@@ -1,3 +1,4 @@
+
 package ttreImages;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -31,28 +32,22 @@ public class TicketDeck {
 			ticketList.add(new Ticket("berlin", "moskva", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/berlin-moskva.png"))));
 			ticketList.add(new Ticket("berlin", "roma", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/berlin-roma.png"))));
 			ticketList.add(new Ticket("brest", "marseille", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/brest-marseille.png"))));
-			ticketList.add(new Ticket("brest", "petrograd", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/brest-petrograd.png"))));
 			ticketList.add(new Ticket("brest", "venezia", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/brest-venezia.png"))));
 			ticketList.add(new Ticket("bruxelles", "danzic", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/bruxelles-danzic.png"))));
 			ticketList.add(new Ticket("budapest", "sofia", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/budapest-sofia.png"))));
-			ticketList.add(new Ticket("cadiz", "stockholm", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/cadiz-stockholm.png"))));
-			ticketList.add(new Ticket("edinburgh", "athina", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/edinburgh-athina.png"))));
 			ticketList.add(new Ticket("edinburgh", "paris", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/edinburgh-paris.png"))));
 			ticketList.add(new Ticket("erzurum", "rostov", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/erzurum-rostov.png"))));
 			ticketList.add(new Ticket("essen", "kyiv", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/essen-kyiv.png"))));
 			ticketList.add(new Ticket("frankfurt", "kobenhavn", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/frankfurt-kobenhavn.png"))));
 			ticketList.add(new Ticket("frankfurt", "smolensk", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/frankfurt-smolensk.png"))));
-			ticketList.add(new Ticket("kobenhavn", "erzurum", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/kobenhavn-erzurum.png"))));
 			ticketList.add(new Ticket("kyiv", "petrograd", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/kyiv-petrograd.png"))));
 			ticketList.add(new Ticket("kyiv", "sochi", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/kyiv-sochi.png"))));
-			ticketList.add(new Ticket("lisboa", "danzig", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/lisboa-danzig.png"))));
 			ticketList.add(new Ticket("london", "berlin", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/london-berlin.png"))));
 			ticketList.add(new Ticket("london", "wien", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/london-wien.png"))));
 			ticketList.add(new Ticket("madrid", "dieppe", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/madrid-dieppe.png"))));
 			ticketList.add(new Ticket("madrid", "zurich", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/madrid-zurich.png"))));
 			ticketList.add(new Ticket("marseille", "essen", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/marseille-essen.png"))));
 			ticketList.add(new Ticket("palermo", "constantinople", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/palermo-constantinople.png"))));
-			ticketList.add(new Ticket("palermo", "moskva", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/palermo-moskva.png"))));
 			ticketList.add(new Ticket("paris", "wien", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/paris-wien.png"))));
 			ticketList.add(new Ticket("paris", "zagrab", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/paris-zagrab.png"))));
 			ticketList.add(new Ticket("riga", "bucuresti", 7, ImageIO.read(MainMenu.class.getResource("/ttreImages/riga-bucuresti.png"))));
