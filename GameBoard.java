a4ff5392905ee304c0c4516c2ad8b491dafa9e10
PauloLeaoCL16 <k2117991@students.katyisd.org>
2025-05-02 11:25:09 -0500

    Update GameBoard.java

--- GameBoard.java
+++ GameBoard.java
@@ -25,7 +25,7 @@ import java.util.*;
 import static java.lang.System.*;
 
 public class GameBoard extends JPanel implements Runnable, MouseListener, MouseMotionListener {
-    private BufferedImage sidewaytemplate, dbPayment,playerGamble ,nextButton, previousButton,table, okbutton, board, player1, player2, player3, player4, cardBack, ticket, template, p1bg, p2bg, p3bg, p4bg, startticket, redplayer, blueplayer, greenplayer, yellowplayer, playerpointer;
+    private BufferedImage sidewaytemplate, greyGambling ,dbPayment,playerGamble ,nextButton, previousButton,table, okbutton, board, player1, player2, player3, player4, cardBack, ticket, template, p1bg, p2bg, p3bg, p4bg, startticket, redplayer, blueplayer, greenplayer, yellowplayer, playerpointer;
     private ColorCard[] faceUpCard;
     private CardDeck cardDeck;
     private Stack<ColorCard> discardDeck;
@@ -38,7 +38,7 @@ public class GameBoard extends JPanel implements Runnable, MouseListener, MouseM
     private int tClicked;
     private int currentPlr;
     private City[] clickedCity = new City[2];
-    private int panelStuff = 0;// 0 = nothing,1 = Start of game,2 = draw tickets,3 = Inventory,4 = Gambling,5 = Grey route payment,6 = Double route payment
+    private int panelStuff = 7;// 0 = nothing,1 = Start of game,2 = draw tickets,3 = Inventory,4 = Gambling,5 = Grey route payment,6 = Double route payment,7 = choosing color for grey Gambling
     private int ticketPage = 1; // 1 = first page, 2 = seconds page, 3 = third page(max)
     private int turnUsed = 0;
     private ArrayList<Ticket> ticketsShownList;
@@ -109,6 +109,7 @@ public class GameBoard extends JPanel implements Runnable, MouseListener, MouseM
             startticket = ImageIO.read(MainMenu.class.getResource("/ttreImages/ticketchoose.png"));
             nextButton = ImageIO.read(MainMenu.class.getResource("/ttreImages/nextButton.png"));
             previousButton = ImageIO.read(MainMenu.class.getResource("/ttreImages/previousButton.png"));
+            greyGambling = ImageIO.read(MainMenu.class.getResource("/ttreImages/greyGambling.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -453,6 +454,24 @@ public class GameBoard extends JPanel implements Runnable, MouseListener, MouseM
                 g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("yellow")), 1757, 890+140); // Yellow
         		
         	}
+        	if( panelStuff == 7)
+        	{
+        		g2d.drawImage(greyGambling,0,0,1880,1070,null);
+        		
+        		g2d.setColor(Color.WHITE);
+        		g2d.setFont(new Font("Serif", Font.BOLD, 30));
+                g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("white")), 1566, 620); // White
+                g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("red")), 1666-15, 620); // Red
+                g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("purple")), 1766-35, 620); // Purple
+                g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("green")), 1866-55, 620); // Green
+                //2nd row
+                g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("brown")), 1566, 821); // Brown
+                g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("blue")), 1666-15, 821); // Blue
+                g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("black")), 1766-35, 821); // Black
+                g2d.drawString(String.valueOf(players.get(currentPlr).getCardColor("yellow")), 1866-55, 821); // Yellow
+        		
+        		
+        	}
         	
         }
         //highlights the ticket clicked 
@@ -513,6 +532,12 @@ public class GameBoard extends JPanel implements Runnable, MouseListener, MouseM
         	int pt = currentPlayer.getPoint()/2;
         	g2d.setPaint(currentPlayer.getPlrColor());
         	int reset = (pt + players.get(i).getStations() * 4) % 100 -12;
+        	if (reset < 0) {
+         		g2d.fillOval(32 - (reset * 47), 960, 30, 30);
+ 	            	g2d.setPaint(new Color(0, 0, 0));
+ 	            	g2d.setStroke(new BasicStroke(2));
+ 	            	g2d.drawOval(32 - (reset * 47), 960, 30, 30);
+         	}
         	if (reset <= 20) {
         		g2d.fillOval(32, 960 - (reset * 46), 30, 30);
             	g2d.setPaint(new Color(0, 0, 0));
@@ -629,6 +654,42 @@ public class GameBoard extends JPanel implements Runnable, MouseListener, MouseM
 				}
 					
 			}
+			
+			if( x>=1539 && x<=1612 && y>=481 && y<=595 && panelStuff == 7  )
+			{
+				//change X-locations of all cards
+			}
+			if( x>=1666-15 && x<=1766-35 && y>=481 && y<=595 && panelStuff == 7  )
+			{
+				
+			}
+			if( x>=1766-35 && x<=1866-55 && y>=481 && y<=595 && panelStuff == 7  )
+			{
+				
+			}
+			if( x>=1866-55 && x<=1966-55 && y>=481 && y<=595 && panelStuff == 7  )
+			{
+				
+			}
+			//2nd row of cards
+			if( x>=1566 && x<=1666-15 && y>=684 && y<=805 && panelStuff == 7  )
+			{
+				
+			}
+			if( x>=1566 && x<=1666-15 && y>=684 && y<=805 && panelStuff == 7  )
+			{
+				
+		
+			}
+			if( x>=1566 && x<=1666-15 && y>=684 && y<=805 && panelStuff == 7  )
+			{
+				
+			}
+			if( x>=1566 && x<=1666-15 && y>=684 && y<=805 && panelStuff == 7  )
+			{
+				
+			}
+			
 		
 			if( x>=1550 && x<=1618 && y>=631 && y<=731 && (panelStuff == 5 || panelStuff == 4) )//wild card
 			{
@@ -1656,6 +1717,8 @@ public class GameBoard extends JPanel implements Runnable, MouseListener, MouseM
     	if (lastTurn == true) {
     		players.get(currentPlr).setLastTurn();
     	}
+    	clickedCity[0] = null;
+     	clickedCity[1] = null;
     	currentPlr += 1;
     	turnUsed = 0;
     	if (currentPlr >= players.size()) {
@@ -1687,7 +1750,22 @@ public class GameBoard extends JPanel implements Runnable, MouseListener, MouseM
     	}
     	if (players.get(currentPlr).getLastTurn() == true) {
     		
-    		out.println("End Game");
+    		Player plr1 = players.get(0);
+         	int pt1 = plr1.getPoint()/2;
+         	int totalPt1 = pt1 + players.get(0).getStations() * 4;
+         	
+         	Player plr2 = players.get(1);
+         	int pt2 = plr2.getPoint()/2;
+         	int totalPt2 = pt2 + players.get(1).getStations() * 4;
+         	
+         	Player plr3 = players.get(2);
+         	int pt3 = plr3.getPoint()/2;
+         	int totalPt3 = pt3 + players.get(2).getStations() * 4;
+         	
+         	Player plr4 = players.get(3);
+         	int pt4 = plr4.getPoint()/2;
+         	int totalPt4 = pt1 + players.get(3).getStations() * 4;
+    		EndGame newEndgame = new EndGame(totalPt1, totalPt2, totalPt3, totalPt4);
     	}
     }
     
