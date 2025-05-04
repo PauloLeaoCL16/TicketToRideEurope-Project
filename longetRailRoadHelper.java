7b57fdfec3882475a8d3aad5f56deae0a6e0760f
Quan <111527496+VuAnhQua@users.noreply.github.com>
2025-05-04 12:40:53 -0500

    Create longetRailRoadHelper.java

---
+++ longetRailRoadHelper.java
@@ -0,0 +1,20 @@
+public class longestRailRoadHelper {
+    private int size;
+    private Player plr;
+    public longestRailRoadHelper(int size, Player plr) {
+        this.size = size;
+        this.plr = plr;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Player getPlr() {
+        return plr;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
